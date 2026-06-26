ce7fdb4af4b56f43cd4c3aae6eb52f2e5ab39ce2
itimameeee <songtaisendao@gmail.com>
2026-06-26 11:55:24 +0900

    User name added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -1,10 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class HeadsOrTails {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int heads = 0, tails = 0;
 
+        System.out.println("Who are you?");
+        System.out.print("> ");
+        String name = scanner.nextLine();
+        
+        System.out.println("Hello, " + name + "!");
+
+        int heads = 0, tails = 0;
         System.out.println("Tossing a coin...");
 
         for (int i = 1; i <= 3; i++) {
